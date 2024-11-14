41431b55ce27a602f870057b7f180924fcf5babe
mfeti <mfeti2175@gmail.com>
2024-11-14 08:02:39 +0300

    fix animation section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,6 @@ import Article from "./Article";
 function Navbar({ isOpen }) {
   return (
     <section
-      data-aos="fade-right"
       className={`bg-gray-100 min-w-[350px] ${
         isOpen ? "block relative" : "lg:block hidden"
       } min-h-screen`}
